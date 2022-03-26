921eb671cf63583a24e93c780baa468406da5f65
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-26 14:09:59 +0900

    footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRotateLeft ,faCircleNotch, faBars  } from '@fortawesome/free-solid-svg-icons';
+const Footer = ({clicked,setClicked}) => {
+    const onCancle = ()=>{
+        setClicked(!clicked);
+    }
+    return(
+        <footer className={styles.footer}>
+            <span className={styles.icon} onClick={onCancle}><FontAwesomeIcon icon={faArrowRotateLeft} /></span>
+            <span className={styles.icon}><FontAwesomeIcon icon={faCircleNotch} /></span>
+            <span className={styles.icon}><FontAwesomeIcon icon={faBars} /></span>
+        </footer>
+    );
+};
+
+export default Footer;
